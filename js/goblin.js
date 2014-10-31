028b5e100f9c0c2874b1dda0934b93941a3d59d4
Alistair Duggin <alistair.duggin@btinternet.com>
2014-10-31 00:08:25 +0000

    Build: 1 Branch: master  0.0.0 SHA: 08acaca

---
+++ js/goblin.js
@@ -0,0 +1,16 @@
+!function(global) {
+  'use strict';
+
+  function Goblin(name) {
+    this.name = name || 'Zomeone';
+  }
+
+  Goblin.prototype.speak = function () {
+    console.log('Waaagh!');
+  };
+
+  global.Goblin = Goblin;
+}(this);
+
+
+
