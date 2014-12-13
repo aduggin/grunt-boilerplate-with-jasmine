799e9f87fe67dcb8262104a713c147f8a85790ec
Alistair Duggin <alistair.duggin@btinternet.com>
2014-12-13 00:13:58 +0000

    Retrieve values from form

--- app/js/InterestCalculatorController.js
+++ app/js/InterestCalculatorController.js
@@ -2,16 +2,33 @@
   'use strict';
 
   function InterestCalculatorController(view) {
-    this.$view = $(view);
+    this.$container = $(view.container);
+    this.$initialValue = $(view.initialValue);
+    this.$years = $(view.years);
+    this.$annualDeposit = $(view.annualDeposit);
+    this.$submit = $(view.submit);
   }
 
   InterestCalculatorController.prototype.init = function () {
-    this.$view.find('input[type=submit]').click(this.submitHandler);
+    this.$container.find(this.$submit ).on('click', this.submitHandler);
   };
 
-  InterestCalculatorController.prototype.submitHandler = function () {
-    console.log( 'Handler for .click() called.');
+  InterestCalculatorController.prototype.submitHandler = function (event) {
+    console.log(event);
   };
+
+  InterestCalculatorController.prototype.getInitialValue = function () {
+    return this.$container.find(this.$initialValue).val();
+  };
+
+  InterestCalculatorController.prototype.getYears = function () {
+    return this.$container.find(this.$years).val();
+  };
+
+  InterestCalculatorController.prototype.getAnnualDeposit = function () {
+    return this.$container.find(this.$annualDeposit).val();
+  };
+
 
   global.InterestCalculatorController = InterestCalculatorController;
 
--- test/spec/InterestCalculatorController_spec.js
+++ test/spec/InterestCalculatorController_spec.js
@@ -1,19 +1,54 @@
 /*globals InterestCalculatorController:false*/
 
-beforeEach(function () {
-  'use strict';
-  loadFixtures('interest-calculator.html');
-});
-
 describe('controller', function () {
   'use strict';
 
-  it('adds click handler to calculate button', function () {
-    var controller = new InterestCalculatorController('#interest-calculator');
-    controller.init();
+  var view, controller;
 
-    var $submit = $('#interest-calculator input[type=submit]');
-    expect($submit).toHandleWith('click', controller.submitHandler);
+  beforeEach(function() {
+    loadFixtures('interest-calculator.html');
+    view = {
+      container: '#interest-calculator',
+      initialValue: '#initial-value',
+      years: '#years',
+      annualDeposit: '#payment',
+      submit: 'input[type=submit]'
+    };
+    controller = new InterestCalculatorController(view);
+  });
+
+  describe('init()', function () {
+    it('adds click handler to calculate button', function () {
+      controller.init();
+      var $submit = $(view.submit);
+
+      expect($submit).toHandleWith('click', controller.submitHandler);
+    });
+  });
+
+
+  describe('getInitialValue()', function () {
+    it('returns the value of the "Initial value" field', function () {
+      $(view.initialValue).val('20000');
+
+      expect(controller.getInitialValue()).toEqual('20000');
+    });
+  });
+
+  describe('getYears()', function () {
+    it('returns the value of the "years" field', function () {
+      $(view.years).val('5');
+
+      expect(controller.getYears()).toEqual('5');
+    });
+  });
+
+  describe('getAnnualDeposit()', function () {
+    it('returns the value of the "annual deposit" field', function () {
+      $(view.annualDeposit).val('5200');
+
+      expect(controller.getAnnualDeposit()).toEqual('5200');
+    });
   });
 
 });
