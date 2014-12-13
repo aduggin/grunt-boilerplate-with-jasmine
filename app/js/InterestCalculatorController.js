ba46d4aea3b129382616645dda3989bd05bdc139
Alistair Duggin <alistair.duggin@btinternet.com>
2014-12-13 11:18:13 +0000

    Add shortcut to prototype

--- app/js/InterestCalculatorController.js
+++ app/js/InterestCalculatorController.js
@@ -1,6 +1,8 @@
 (function(global) {
   'use strict';
 
+  var proto;
+
   function InterestCalculatorController(view) {
     this.$container = $(view.container);
     this.$initialValue = $(view.initialValue);
@@ -9,23 +11,25 @@
     this.$submit = $(view.submit);
   }
 
-  InterestCalculatorController.prototype.init = function () {
+  proto = InterestCalculatorController.prototype;
+
+  proto.init = function () {
     this.$container.find(this.$submit ).on('click', this.submitHandler);
   };
 
-  InterestCalculatorController.prototype.submitHandler = function (event) {
+  proto.submitHandler = function (event) {
     console.log(event);
   };
 
-  InterestCalculatorController.prototype.getInitialValue = function () {
+  proto.getInitialValue = function () {
     return this.$container.find(this.$initialValue).val();
   };
 
-  InterestCalculatorController.prototype.getYears = function () {
+  proto.getYears = function () {
     return this.$container.find(this.$years).val();
   };
 
-  InterestCalculatorController.prototype.getAnnualDeposit = function () {
+  proto.getAnnualDeposit = function () {
     return this.$container.find(this.$annualDeposit).val();
   };
 
